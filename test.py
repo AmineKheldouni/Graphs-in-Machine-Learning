b16cd1acc77aec0bb8336a77699f279b5c52a1ec
AmineKheldouni <aminekheldouni@gmail.com>
2018-12-25 11:31:44 +0100

    Added demo files for graph_nets and a test file for DM Control environment viewer.

---
+++ test.py
@@ -0,0 +1,36 @@
+import time
+
+import graph_nets
+from matplotlib import pyplot as plt
+import numpy as np
+# import pandas
+import sonnet as snt
+import tensorflow as tf
+import gym
+from dm_control import suite
+from dm_control import viewer
+print("Imported libraries !")
+
+SEED = 1
+np.random.seed(SEED)
+tf.set_random_seed(SEED)
+
+
+print("Creating environment ...")
+# Load one task:
+env = suite.load(domain_name="cartpole", task_name="swingup")
+
+print("Environment created !")
+
+# Action set
+action_spec = env.action_spec()
+# Define a uniform random policy.
+def random_policy(time_step):
+  del time_step  # Unused.
+  return np.random.uniform(low=action_spec.minimum,
+                           high=action_spec.maximum,
+                           size=action_spec.shape)
+
+print("Rendering the environment ...")
+# Launch the viewer application.
+viewer.launch(env, policy=random_policy)
